d0e648a2f392d56b6c3e8155e2a8fe62a3e435df
pavansai018 <18pavansai@gmail.com>
2025-12-19 16:29:21 +0800

    dot product of multiple neurons with numpy

---
+++ src/multi_neuron_v2.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def multi_neuron(inputs, weights, biases):
+    '''
+    Docstring for multi_neuron
+    
+    :param inputs: inputs
+    :param weights: weights
+    :param bias: biases
+
+    Returns:
+    -------------
+    weighted_sum
+    '''
+    weighted_sum = []
+    for neuron_weight, bias in zip(weights, biases):
+        output = np.dot(inputs, neuron_weight) + bias
+        weighted_sum.append(output)
+    return np.asarray(weighted_sum)
+
+if __name__ == '__main__':
+    i = [1, 2, 3, 2.5]
+    w = [   [0.2, 0.8, -0.5, 1],
+            [0.5, -0.91, 0.26,-0.5],
+            [-0.26, -0.27, 0.17,0.87]]
+    b = [2, 3, 0.5]
+    print(f'Weighted Sum: {multi_neuron(i, w, b)}')
